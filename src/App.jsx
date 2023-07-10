3168c6b9c4e9af4ac8b2e49f3bb39d6c2310f692
Bhuwanxhettri <you@example.com>
2023-07-11 01:01:05 +0545

    outlet in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import { useState } from 'react'
-
+import { Routes, Route, Outlet } from 'react-router'
 import './App.css'
+import Home from './routes/home.component';
+
+const NavBar = () => {
+  return <>
+    <h3>Navigation bar</h3>
+    <Outlet />
+  </>
+}
+
+const Shop = () => {
+  return <>
+    <h1>Shop component</h1>
+
+  </>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path='/' element={< NavBar />} >
+          <Route index element={<Home />} />
+          <Route path='shop' element={<Shop />} />
+        </Route>
+      </Routes>
     </>
   )
 }
